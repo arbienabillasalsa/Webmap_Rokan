b0f85a809065dbba7aecc5796ef1526c93898985
Arbie Nabillasalsa <43590287+arbienabillasalsa@users.noreply.github.com>
2021-11-18 20:04:22 +0700

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,15 @@
+// Where to load data from. Choose either a local file in GitHub repo, such as `data/places.csv`,
+// or a Google Sheets URL (File > Publish to the Web > CSV )
+const dataLocation = 'data/KPH_Rokan.csv';
+
+// Map's initial center, and zoom level
+const mapCenter = [0.865842, 100.300172];
+const mapZoom = 12;
+
+// Marker icon height and width
+const iconHeight = 30;
+const iconWidth = 30;
+
+//Icon for pohon muda and pohon dewasa
+const pohonDewasa =  'logo/mature_tree.svg'
+const pohonMuda = 'logo/young_tree.svg'
